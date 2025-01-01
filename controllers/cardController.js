3638e9a251c30410f355325f27d72f10901999c3
PashkattFromUA <pashkattfromua@gmail.com>
2025-01-01 15:27:00 +0100

    localised res for cards

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -43,7 +43,7 @@ export const updateCard = async (req, res) => {
         card.image.split("/").pop()
       );
       if (await fsPromises.stat(oldImagePath).catch(() => false)) {
-        await fsPromises.unlink(oldImagePath); // Удаляем старый файл
+        await fsPromises.unlink(oldImagePath); 
       }
       newImagePath = `/uploads/${files.image[0].filename}`;
     }
@@ -55,7 +55,7 @@ export const updateCard = async (req, res) => {
         card.icon.split("/").pop()
       );
       if (await fsPromises.stat(oldIconPath).catch(() => false)) {
-        await fsPromises.unlink(oldIconPath); // Удаляем старый файл
+        await fsPromises.unlink(oldIconPath); 
       }
       newIconPath = `/uploads/${files.icon[0].filename}`;
     }
@@ -115,13 +115,32 @@ export const deleteCard = async (req, res) => {
 export const getCardById = async (req, res) => {
   try {
     const { id } = req.params;
+    const locale = req.headers["accept-language"] || "en";
     const card = await Card.findById(id);
 
     if (!card) {
       return res.status(404).json({ message: "Card not found" });
     }
 
-    res.status(200).json(card);
+    const localizedCard = {
+      category: card.category[locale] || card.category["en"],
+      place: card.place[locale] || card.place["en"],
+      shortDescription: card.shortDescription[locale] || card.shortDescription["en"],
+      description: card.description[locale] || card.description["en"],
+      features: card.features[locale] || card.features["en"],
+      seo: card.seo[locale] || card.seo["en"],
+      slug: card.slug,
+      id: card.id,
+      name: card.name,
+      rating: card.rating,
+      ratingUrl: card.ratingUrl,
+      link: card.link,
+      icon: card.icon,
+      image: card.image,
+    };
+    console.log(localizedCard);
+
+    res.status(200).json(localizedCard);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -129,8 +148,28 @@ export const getCardById = async (req, res) => {
 
 export const getAllCards = async (req, res) => {
   try {
+    const locale = req.headers["accept-language"] || "en";
+
     const cards = await Card.find();
-    return res.status(200).json(cards);
+
+    const localizedCards = cards.map((card) => ({
+      category: card.category[locale] || card.category["en"],
+      place: card.place[locale] || card.place["en"],
+      shortDescription: card.shortDescription[locale] || card.shortDescription["en"],
+      description: card.description[locale] || card.description["en"],
+      features: card.features[locale] || card.features["en"],
+      seo: card.seo[locale] || card.seo["en"],
+      slug: card.slug,
+      id: card.id,
+      name: card.name,
+      rating: card.rating,
+      ratingUrl: card.ratingUrl,
+      link: card.link,
+      icon: card.icon,
+      image: card.image,
+    }));
+    console.log(localizedCards);
+    return res.status(200).json(localizedCards);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
